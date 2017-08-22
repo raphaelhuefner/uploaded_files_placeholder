0d6941c85bf240206e4d852a5262a9b3946ab435
Raphael Huefner <raphael@affinitybridge.com>
2017-08-21 17:00:28 -0700

    add BC shim for imagepalettetotruecolor()

--- src/Strategy/SymlinkImage/Jpg.php
+++ src/Strategy/SymlinkImage/Jpg.php
@@ -5,6 +5,25 @@ namespace Raphaelhuefner\UploadedFilesPlaceholder\Strategy\SymlinkImage;
 use Raphaelhuefner\UploadedFilesPlaceholder\StrategyInterface as StrategyInterface;
 use Raphaelhuefner\UploadedFilesPlaceholder\Strategy\SymlinkImage as SymlinkImage;
 
+// @see http://www.php.net/manual/en/function.imagepalettetotruecolor.php
+// Backwards compatiblity
+if (!function_exists('imagepalettetotruecolor')) {
+    function imagepalettetotruecolor(&$src) {
+        if(imageistruecolor($src)) {
+            return(true);
+        }
+
+        $dst = imagecreatetruecolor(imagesx($src), imagesy($src));
+
+        imagecopy($dst, $src, 0, 0, 0, 0, imagesx($src), imagesy($src));
+        imagedestroy($src);
+
+        $src = $dst;
+
+        return TRUE;
+    }
+}
+
 class Jpg extends SymlinkImage implements StrategyInterface {
     public function handle(array $file, $sourceDirectory, $destinationDirectory) {
         if ('image/jpeg' == $file['mimeType']) {
